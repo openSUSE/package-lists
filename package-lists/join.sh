2ffebc0e8fe22cb6e541bc1a616161785d7083c6
Christoph Thiel <cthiel@suse.de>
2007-09-11 14:53:31 +0000

    improve join.sh

--- package-lists/join.sh
+++ package-lists/join.sh
@@ -3,6 +3,9 @@
 for i in i586 x86_64 ppc;
 do
   cat dvd5.$i.list dvd5-2.$i.list | LANG=C sort -u > dvd-all.$i.list
-
 done
+
+LANG=C sort -u kde-cd.*.list  > kde-cd-all.list
+LANG=C sort -u gnome-cd.*.list > gnome-cd-all.list
+
 cat dvd-all.*.list | LANG=C sort -u > dvd-all.list
