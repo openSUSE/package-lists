186444860c63d447585242b286221dfbe8ae22b3
Christoph Thiel <cthiel@suse.de>
2007-09-13 13:49:34 +0000

    vastly improve doit.sh

--- package-lists/doit.sh
+++ package-lists/doit.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+echo "dvd5:"
 ./gen2.sh dvd5
 ./gen2.sh dvd5-2
 
+echo "language addon:"
 ./gen2.sh dvd5-addon_lang
 
+echo "kde-cd:"
 ./gen2.sh kde-cd
 ./gen2.sh kde-cd-non_oss
+echo "gnome-cd:"
 ./gen2.sh gnome-cd
 ./gen2.sh gnome-cd-non_oss
 
 ./join.sh
 ./non_oss.sh
 ./langaddon.sh
-
