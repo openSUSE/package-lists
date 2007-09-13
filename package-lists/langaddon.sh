eb85f6f91c175d8d4d7fca7bb6abc2f8cf9b7460
Christoph Thiel <cthiel@suse.de>
2007-09-13 17:09:46 +0000

    lang addon fixes + beta3+

--- package-lists/langaddon.sh
+++ package-lists/langaddon.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo -n > langaddon.list
+echo -n > langaddon-all.list
 
 for i in i586 x86_64 ppc; do
-  diff dvd5.$i.list dvd5-addon_lang.$i.list | grep '^>' >> langaddon.list
+  diff dvd5.$i.list dvd5-addon_lang.$i.list | grep '^>' >> langaddon-all.list
 done
-LANG=C sort -u langaddon.list | cut -d" " -f2 > langaddon.list.sorted && mv langaddon.list.sorted langaddon.list
+LANG=C sort -u langaddon-all.list | cut -d" " -f2 > langaddon-all.list.sorted && mv langaddon-all.list.sorted langaddon-all.list
