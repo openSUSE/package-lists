959f043f4d648cfaebfd5d2a031320f6130fe885
Christoph Thiel <cthiel@suse.de>
2007-09-13 13:50:24 +0000

    reorder doit.sh

--- package-lists/doit.sh
+++ package-lists/doit.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-echo "dvd5:"
-./gen2.sh dvd5
-./gen2.sh dvd5-2
-
-echo "language addon:"
-./gen2.sh dvd5-addon_lang
-
 echo "kde-cd:"
 ./gen2.sh kde-cd
 ./gen2.sh kde-cd-non_oss
@@ -14,6 +7,15 @@ echo "gnome-cd:"
 ./gen2.sh gnome-cd
 ./gen2.sh gnome-cd-non_oss
 
-./join.sh
 ./non_oss.sh
+./join.sh
+
+echo "dvd5:"
+./gen2.sh dvd5
+./gen2.sh dvd5-2
+
+echo "language addon:"
+./gen2.sh dvd5-addon_lang
+
+./join.sh
 ./langaddon.sh
