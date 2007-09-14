93511905ac6ca230e27fb6d5a1ca3c50220e1e48
Christoph Thiel <cthiel@suse.de>
2007-09-14 14:56:20 +0000

    update scripts

--- package-lists/doit.sh
+++ package-lists/doit.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 echo "kde-cd:"
-./gen2.sh kde-cd
-./gen2.sh kde-cd-non_oss
+./gen.sh kde-cd
+./gen.sh kde-cd-non_oss
 echo "gnome-cd:"
-./gen2.sh gnome-cd
-./gen2.sh gnome-cd-non_oss
+./gen.sh gnome-cd
+./gen.sh gnome-cd-non_oss
 
 ./non_oss.sh
 ./join.sh
 
 echo "dvd5:"
-./gen2.sh dvd5
-./gen2.sh dvd5-2
+./gen.sh dvd5
+./gen.sh dvd5-2
 
 echo "language addon:"
-./gen2.sh dvd5-addon_lang
+./gen.sh dvd5-addon_lang
 
 ./join.sh
 ./langaddon.sh
--- package-lists/gen.sh
+++ package-lists/gen.sh
@@ -1,6 +1,28 @@
-#! /bin/sh
+#!/bin/sh
+
+GEN_ARCH="i586 x86_64 ppc"
+BASEDIR=`pwd`
+GEN_URL_i586="$BASEDIR/testtrack/full-i386"
+GEN_URL_x86_64="$BASEDIR/testtrack/full-x86_64"
+GEN_URL_ppc="$BASEDIR/testtrack/full-ppc"
+TESTTRACK="`pwd`/testtrack/"
+
+if test -f config.sh; then
+  . config.sh
+fi
 
 file=$1
+
+
+# no ppc cds
+if (echo $file | grep "cd" > /dev/null); then
+  GEN_ARCH=${GEN_ARCH/ppc/}
+  base=$file
+else
+  # multiple dvd setups (dvd5, dvd5-2, etc.)
+  base=${file/-*/}
+fi
+
 for pack in `pdb query --filter status:internal` `pdb query --filter status:candidate` `pdb query --filter status:frozen`; do
   grep -x $pack overwrites && continue
   LOCK="$LOCK <lock package=\"$pack\"/>"
@@ -14,9 +36,29 @@ for pack in `pdb query --filter status:internal` `pdb query --filter status:cand
   esac
 done
 
-sed -e "s,<!-- INTERNALS -->,$LOCK," $file.xml.in | xmllint --format - > $file.xml
+for i in $GEN_ARCH;
+do
+  arch=$i
+  echo "processing $arch..."
+  eval VAR="\$GEN_URL_${i}"
+  sed -e "s,<!-- INTERNALS -->,$LOCK," -e "s,GEN_ARCH,$i," -e "s,GEN_URL,$VAR," $file.xml.in | grep -v "!$arch" | xmllint --format - > $file.$arch.xml
 
-echo "done"
+  rm -rf /tmp/myrepos/*
+  cp -av $VAR/* $TESTTRACK/$base.$arch/
+  
+  pushd $VAR/suse/setup/descr/ > /dev/null
+  for i in *; 
+    do echo -n "META SHA1 "; 
+    sha1sum $i | awk '{ORS=""; print $1}'; 
+    echo -n " "; basename $i; 
+  done >> $TESTTRACK/$base.$arch/content
+  popd > /dev/null
+  gpg  --batch -a -b --sign $TESTTRACK/$base.$arch/content
 
-/usr/lib/zypp/testsuite/bin/deptestomatic.multi $file.xml 2> $file.error | tee $file.output | sed -n -e '1,/Other Valid Solution/p' | grep -v 'install pattern:' | grep -v 'install product:' | grep "> install.*\[tmp\]"  | sed -e 's,>!> install \(.*\)-[^-]*-[^-]*$,\1,' | LC_ALL=C sort -u -o $file.list -
+  /usr/lib/zypp/testsuite/bin/deptestomatic.multi $file.$arch.xml 2> $file.$arch.error | tee $file.$arch.output | sed -n -e '1,/Other Valid Solution/p' | grep -v 'install pattern:' | grep -v 'install product:' | grep "> install.*\[tmp\]"  | sed -e 's,>!> install \(.*\)-[^-]*-[^-]*$,\1,' | LC_ALL=C sort -u -o $file.$arch.list -
 
+  echo "done"
+done
+
+rm -rf /tmp/myrepos/*
+echo "all done"
