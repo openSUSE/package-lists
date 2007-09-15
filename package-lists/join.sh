977cec332ac4f3b788f91fcbd1d098617ef69d7e
Christoph Thiel <cthiel@suse.de>
2007-09-15 19:41:06 +0000

    LANG is not enough

--- package-lists/join.sh
+++ package-lists/join.sh
@@ -2,10 +2,10 @@
 
 for i in i586 x86_64 ppc;
 do
-  cat dvd5.$i.list dvd5-2.$i.list | LANG=C sort -u > dvd-all.$i.list
+  cat dvd5.$i.list dvd5-2.$i.list | LC_ALL=C sort -u > dvd-all.$i.list
 done
 
-LANG=C sort -u kde-cd.*.list  > kde-cd-all.list
-LANG=C sort -u gnome-cd.*.list > gnome-cd-all.list
+LC_ALL=C sort -u kde-cd.*.list  > kde-cd-all.list
+LC_ALL=C sort -u gnome-cd.*.list > gnome-cd-all.list
 
-cat dvd-all.*.list | LANG=C sort -u > dvd-all.list
+cat dvd-all.*.list | LC_ALL=C sort -u > dvd-all.list
