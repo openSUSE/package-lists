6c5dae1a3b8fddb1f334356d7a619b004068c16d
Stephan Kulow <coolo@suse.de>
2007-12-22 13:31:37 +0000

    silence

--- package-lists/testtrack/update_full.sh
+++ package-lists/testtrack/update_full.sh
@@ -11,8 +11,10 @@ do
   pushd full-$i > /dev/null
 # ln -s /work/CDs/all/full-$i/suse susex
   mkdir -p .cache
+  echo -n "create_package_descr $i "
   /work/cd/bin/tools/create_package_descr -c .cache -i /work/cd/lib/put_built_to_cd/locations-stable/meta/ \
     -i /work/cd/lib/put_built_to_cd/locations-stable/debug/ \
-    -P -C -K -S -x /work/built/dists/all/$i/data/EXTRA_PROV -o suse/setup/descr/ -d susex/ -l english
+    -P -C -K -S -x /work/built/dists/all/$i/data/EXTRA_PROV -o suse/setup/descr/ -d susex/ -l english > /dev/null 2>&1
+  echo "done"
   popd > /dev/null
 done
