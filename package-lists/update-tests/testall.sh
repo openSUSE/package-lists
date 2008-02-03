3a7d5a9393498b4aace4a718130a6d7c1689c1cb
Stephan Kulow <coolo@suse.de>
2008-02-03 17:12:03 +0000

    more debug

--- package-lists/update-tests/testall.sh
+++ package-lists/update-tests/testall.sh
@@ -1,7 +1,13 @@
 for i in *.xml.in; do 
 echo $i
-./update.sh $i > list.update
+./update.sh $i > $i.update
+if test ! -s $i.update; then
+  grep -C3 === $i.output
+  : > $i.remove
+  continue
+fi
+
 sed -e 's,delete_unmaintained="false",delete_unmaintained="true",' $i > ${i/update.xml.in/remove.xml}
-./update.sh ${i/update.xml.in/remove.xml} > list.remove
-diff -u list.update list.remove | grep '^[-+]'
+./update.sh ${i/update.xml.in/remove.xml} > $i.remove
+diff -u $i.update $i.remove | grep '^[-+]'
 done
--- package-lists/update-tests/update.sh
+++ package-lists/update-tests/update.sh
@@ -1,2 +1,3 @@
-/usr/lib/zypp/testsuite/bin/deptestomatic.multi $1 2> 10.2-i386.error | tee 10.2-i386.output | sed -n -e '1,/Other Valid Solution/p' | grep -v ' pattern:' | grep -v 'install product:' | grep '^>!>' | grep -e '^>!> \(install\|remove\|upgrade\) ' | sed -e 's,^>!> ,,; s, => .*,,; s,\[factor.*\].*,,; s,-[^-]*-[^-]*\.\(i586\|noarch\)$,,'
+rm -rf /tmp/myrepos
+/usr/lib/zypp/testsuite/bin/deptestomatic.multi $1 2> $1.error | tee $1.output | sed -n -e '1,/Other Valid Solution/p' | grep -v ' pattern:' | grep -v 'install product:' | grep '^>!>' | grep -e '^>!> \(install\|remove\|upgrade\) ' | sed -e 's,^>!> ,,; s, => .*,,; s,\[factor.*\].*,,; s,-[^-]*-[^-]*\.\(i586\|noarch\)$,,'
 
