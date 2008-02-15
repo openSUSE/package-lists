c864c884713ab027229bdb9433b54c3c058f13cc
Stephan Kulow <coolo@suse.de>
2008-02-15 08:01:32 +0000

    updates

--- package-lists/join.sh
+++ package-lists/join.sh
@@ -5,7 +5,4 @@ do
   cat dvd5.$i.list dvd5-2.$i.list | LC_ALL=C sort -u > dvd-all.$i.list
 done
 
-LC_ALL=C sort -u kde-cd.*.list  > kde-cd-all.list
-LC_ALL=C sort -u gnome-cd.*.list > gnome-cd-all.list
-
 cat dvd-all.*.list | LC_ALL=C sort -u > dvd-all.list
