c09d46da88261ddea6cd22080097f9064aa46273
Stephan Kulow <coolo@suse.de>
2008-03-18 16:26:30 +0000

    do not hardcode arch

--- package-lists/prepare_patterns.sh
+++ package-lists/prepare_patterns.sh
@@ -2,9 +2,14 @@ cd /home/pattern
 svn up
 export RPM_SOURCE_DIR=$PWD
 cd products/patterns-openSUSE-data
-for i in `cd toinstall && ls -1d *`; do mkdir -p utf8_summary/$i/; echo "=Sum: Nada" >  utf8_summary/$i/default; mkdir -p utf8_description/$i/; echo "+Des:" >  utf8_description/$i/default; echo "nada" >> utf8_description/$i/default; echo "-Des:" >>  utf8_description/$i/default; done
+for i in `cd toinstall && ls -1d *`; do 
+   mkdir -p utf8_summary/$i/
+   echo "=Sum: Nada" >  utf8_summary/$i/default
+   mkdir -p utf8_description/$i/ 
+   ( echo "+Des:"; echo "nada" ; echo "-Des:" ) >  utf8_description/$i/default
+done
 rm -rf /tmp/patterns
-export RPM_BUILD_ROOT=/tmp/patterns
-export EXPLICIT_UNAME=i586
-sh -x $RPM_SOURCE_DIR/sort_opensuse_patterns 11.0 1 i586
+export RPM_BUILD_ROOT=/tmp/patterns.$1
+export EXPLICIT_UNAME=$1
+sh -x $RPM_SOURCE_DIR/sort_opensuse_patterns 11.0 1 $1
 
