a3887b13d458738ea0bd479484590daf9b3bfa70
Stephan Kulow <coolo@suse.de>
2008-05-18 10:12:50 +0000

    cleanup more

--- package-lists/testtrack/unpack_patterns.sh
+++ package-lists/testtrack/unpack_patterns.sh
@@ -10,9 +10,9 @@ sh ../prepare_patterns.sh ppc ppc
 
 
 for arch in i586 x86_64 ppc; do
-for i in kde_cd kde_cd_non_oss gnome_cd gnome_cd_non_oss dvd5;
+for i in kde4_cd kde3_cd kde4_cd_non_oss kde3_cd_non_oss gnome_cd gnome_cd_non_oss dvd5;
 do
-  rm -rf $i.$arch/CD1
+  rm -rf $i.$arch
 done
 done
 
@@ -25,7 +25,7 @@ copy()
     cp -a $file $2/$dir
 }
 
-for flav in kde gnome; do
+for flav in kde3 kde4 gnome; do
   for arch in i586 x86_64 ppc; do
     copy /tmp/patterns.$arch/CD1/suse/setup/descr/"$flav"_cd-*.pat "$flav"_cd.$arch
 
