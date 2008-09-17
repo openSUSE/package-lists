53142cf02e31587eca896c2bf760423b150773e1
Christoph Thiel <cthiel@suse.de>
2008-09-17 11:48:40 +0000

    moving to head

--- package-lists/update-tests/testall.sh
+++ package-lists/update-tests/testall.sh
@@ -1,13 +1,13 @@
 export LC_ALL=C
 
 rm -rf /tmp/myrepos
-rm -rf full-i386 full-x86_64
+rm -rf full-head-i586 full-head-x86_64
 /usr/lib/zypp/testsuite/bin/deptestomatic.multi testit-x86_64.xml > testit-x86_64.log 2>&1
 rm -rf /tmp/myrepos
 /usr/lib/zypp/testsuite/bin/deptestomatic.multi testit-i386.xml > testit-i386.log 2>&1
 
-zcat full-i386/*-package.xml.gz | fgrep -v '<vendor>' > full-i386/1-package.xml
-zcat full-x86_64/*-package.xml.gz | fgrep -v '<vendor>' > full-x86_64/1-package.xml
+zcat full-head-i586/*-package.xml.gz | fgrep -v '<vendor>' > full-head-i586/1-package.xml
+zcat full-head-x86_64/*-package.xml.gz | fgrep -v '<vendor>' > full-head-x86_64/1-package.xml
 
 for i in *-update.xml; do 
   echo $i
