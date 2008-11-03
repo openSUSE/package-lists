8847dc46b8f780351364611a0f9efb6779c53375
Stephan Kulow <coolo@suse.de>
2008-11-03 07:18:08 +0000

    "no more automatic diffs for sled"

--- package-lists/difflist.sh
+++ package-lists/difflist.sh
@@ -6,11 +6,12 @@ difflist()
    diff -u autobuild-lists/$1 $1 | grep '^-' | grep -v '^[-+][-+]' | LC_ALL=C sort | grep -v 64bit | cut -b2- > /package_lists/"$2"-
 }
          
-difflist dvd-all.list suse111-dvd5-i386
-difflist langaddon-all.list suse111-cd-lang-i386
-difflist sled-all.list sled11-dvd-i386
+#difflist dvd-all.list suse111-dvd5-i386
+#difflist langaddon-all.list suse111-cd-lang-i386
+#difflist sled-all.list sled11-dvd-i386
 #difflist promo_dvd.i586.list suse110-dvd5-promo-i386
 
 diff -u autobuild-lists/sles-all.list sles-all.list > /package_lists/purely_for_kukuk
+diff -u autobuild-lists/sled-all.list sled-all.list > /package_lists/purely_for_behlert
 difflist sdk-all.list sles11-sdk-dvd-i386
 
