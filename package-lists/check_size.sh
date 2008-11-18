2ba78e7f982885179347c3de885f190d2affbd05
Stephan Kulow <coolo@suse.de>
2008-11-18 20:02:05 +0000

    move to one file

--- package-lists/check_size.sh
+++ package-lists/check_size.sh
@@ -1,6 +1,11 @@
 c=0 
 for i in `cat $1 `; do 
-   s=`stat -c %s testtrack/full-head-$2/susex/*/$i.rpm | head -n 1`; 
+   list=`ls -1 testtrack/full-head-$2/susex/*/$i.rpm 2> /dev/null | head -n 1`
+   if test -f "$list"; then
+      s=`stat -c %s $list`
+   else
+      s=0
+   fi
    c=$(($c+$s)); 
 done
 echo $1 $(($c/1048576))
