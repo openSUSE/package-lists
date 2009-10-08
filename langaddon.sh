fab341b16dae32986b50dabebe5617d34b47cdb1
Stephan Kulow <coolo@suse.de>
2009-10-08 07:50:51 +0000

    Revert "remove the opensuse/"
    This reverts commit c576a4cab3ca5354f993339e601028478af40d7a.

--- langaddon.sh
+++ langaddon.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo -n > output/langaddon-all.list
+echo -n > output/opensuse/langaddon-all.list
 
 for i in i586 x86_64; do
-  diff output/dvd-1.$i.list output/dvd-addon_lang.$i.list | grep '^>' >> output/langaddon-all.list
+  diff output/opensuse/dvd-1.$i.list output/opensuse/dvd-addon_lang.$i.list | grep '^>' >> output/opensuse/langaddon-all.list
 done
-LC_ALL=C sort -u output/langaddon-all.list | cut -d" " -f2 > t && mv t output/langaddon-all.list
+LC_ALL=C sort -u output/opensuse/langaddon-all.list | cut -d" " -f2 > t && mv t output/opensuse/langaddon-all.list
