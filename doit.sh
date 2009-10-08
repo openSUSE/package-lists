332249fd6a22ff2d3b9693db2c4751739c665528
root <root@desdemona.suse.de>
2009-10-08 05:23:16 +0000

    remove the opensuse/

--- doit.sh
+++ doit.sh
@@ -11,7 +11,7 @@ do_opensuse()
     ./gen.sh opensuse/dvd-2
     ./gen.sh opensuse/dvd-3
     for i in i586 x86_64; do
-      cat output/opensuse/dvd-1.$i.list output/opensuse/dvd-2.$i.list output/opensuse/dvd-3.$i.list | LC_ALL=C sort -u > output/opensuse/dvd-$i.list
+      cat output/dvd-1.$i.list output/dvd-2.$i.list output/dvd-3.$i.list | LC_ALL=C sort -u > output/dvd-$i.list
     done
     ./gen.sh opensuse/kde4_cd-base-default
     ./gen.sh opensuse/gnome_cd-x11-default
@@ -20,18 +20,18 @@ do_opensuse()
 
     echo "diffing"
     for arch in i586 x86_64; do
-       diff -u output/opensuse/kde4_cd.$arch.list output/opensuse/kde4_cd-default.$arch.list | grep -v +++ | grep ^+
-       diff -u output/opensuse/gnome_cd.$arch.list output/opensuse/gnome_cd-default.$arch.list | grep -v +++ | grep ^+
+       diff -u output/kde4_cd.$arch.list output/kde4_cd-default.$arch.list | grep -v +++ | grep ^+
+       diff -u output/gnome_cd.$arch.list output/gnome_cd-default.$arch.list | grep -v +++ | grep ^+
     done
 
     for arch in i586 x86_64; do
       for f in gnome_cd-default kde4_cd-default gnome_cd-x11-default kde4_cd-base-default; do
          for i in kernel-default powersave suspend OpenOffice_org-icon-themes smartmontools gtk-lang gimp-lang vte-lang icewm-lite yast2-trans-en_US bundle-lang-common-en opensuse-manual_en bundle-lang-kde-en bundle-lang-gnome-en openSUSE-release openSUSE-release-ftp kernel-default-base kernel-default-extra smolt virtualbox-ose-kmp-default ndiswrapper-kmp-default preload-kmp-default tango-icon-theme oxygen-icon-theme mono-core marble-data; do
-	  grep -vx $i output/opensuse/$f.$arch.list > t && mv t output/opensuse/$f.$arch.list
+	  grep -vx $i output/$f.$arch.list > t && mv t output/$f.$arch.list
 	done
-        grep -v patterns-openSUSE output/opensuse/$f.$arch.list > t && mv t output/opensuse/$f.$arch.list
+        grep -v patterns-openSUSE output/$f.$arch.list > t && mv t output/$f.$arch.list
       done
-      grep -vx openSUSE-release-ftp output/opensuse/x11_cd-initrd.$arch.list > t && mv t output/opensuse/x11_cd-initrd.$arch.list
+      grep -vx openSUSE-release-ftp output/x11_cd-initrd.$arch.list > t && mv t output/x11_cd-initrd.$arch.list
     done
 
     ./gen.sh opensuse/promo_dvd
--- doitall.sh
+++ doitall.sh
@@ -5,9 +5,9 @@ svn up
 diffonly=$1
 if test -z "$diffonly" || test -d "$diffonly"; then
    (cd osc/openSUSE\:Factory/_product/ && osc up)
-   : > output/opensuse/frozen.xml
+   : > output/frozen.xml
    for i in `grep "package name=" osc/openSUSE\:Factory/_product/FROZEN.group | cut -d\" -f2`; do
-      echo "<lock package='$i'/>" >> output/opensuse/frozen.xml
+      echo "<lock package='$i'/>" >> output/frozen.xml
    done
    cd testtrack/
    ./update_full.sh obs-i586 obs-x86_64 # obs-ppc
@@ -28,7 +28,7 @@ cd ..
 diff=0
 for arch in i586 x86_64; do
   for f in gnome_cd-default gnome_cd kde4_cd-default kde4_cd gnome_cd-x11-default kde4_cd-base-default x11_cd; do
-     if ! diff -u saved/$f.$arch.list output/opensuse/$f.$arch.list ; then
+     if ! diff -u saved/$f.$arch.list output/$f.$arch.list ; then
         diff=1 
         break
      fi
@@ -38,27 +38,27 @@ done
 
 if test "$diff" = 1; then
    echo "no diff"
-   tar cjf /package_lists/filelists.tar.bz2 output/opensuse/*_cd*.list
-   cp output/opensuse/*.list saved
+   tar cjf /package_lists/filelists.tar.bz2 output/*_cd*.list
+   cp output/*.list saved
 fi
 
 set -e
 
-./check_yast.sh output/opensuse/dvd-i586.list __i386__
-./check_yast.sh output/opensuse/dvd-x86_64.list __x86_64__
-./check_yast.sh output/opensuse/dvd-ppc.list __powerpc__
+./check_yast.sh output/dvd-i586.list __i386__
+./check_yast.sh output/dvd-x86_64.list __x86_64__
+./check_yast.sh output/dvd-ppc.list __powerpc__
 
 (
-./check_size.sh output/opensuse/dvd-i586.list i586
-./check_size.sh output/opensuse/dvd-x86_64.list x86_64
-./check_size.sh output/opensuse/dvd-ppc.list ppc
+./check_size.sh output/dvd-i586.list i586
+./check_size.sh output/dvd-x86_64.list x86_64
+./check_size.sh output/dvd-ppc.list ppc
 ) | tee sizes
 
-./mk_group.sh output/opensuse/dvd-ppc.list DVD-ppc osc/openSUSE\:Factory/_product/DVD5-ppc.group only_ppc
-./mk_group.sh output/opensuse/dvd-i586.list DVD-i586 osc/openSUSE\:Factory/_product/DVD5-i586.group only_i586
-./mk_group.sh output/opensuse/dvd-x86_64.list DVD-x86_64 osc/openSUSE\:Factory/_product/DVD5-x86_64.group only_x86_64
-./mk_group.sh output/opensuse/promo_dvd.i586.list REST-DVD-promo-i386 osc/openSUSE\:Factory/_product/DVD5-promo-i386.group
-./mk_group.sh output/opensuse/langaddon-all.list REST-DVD osc/openSUSE\:Factory/_product/DVD5-lang.group
+./mk_group.sh output/dvd-ppc.list DVD-ppc osc/openSUSE\:Factory/_product/DVD5-ppc.group only_ppc
+./mk_group.sh output/dvd-i586.list DVD-i586 osc/openSUSE\:Factory/_product/DVD5-i586.group only_i586
+./mk_group.sh output/dvd-x86_64.list DVD-x86_64 osc/openSUSE\:Factory/_product/DVD5-x86_64.group only_x86_64
+./mk_group.sh output/promo_dvd.i586.list REST-DVD-promo-i386 osc/openSUSE\:Factory/_product/DVD5-promo-i386.group
+./mk_group.sh output/langaddon-all.list REST-DVD osc/openSUSE\:Factory/_product/DVD5-lang.group
 ( cd osc/openSUSE\:Factory/_product/ && osc ci -m "auto update" )
 
 if false; then
@@ -69,7 +69,7 @@ if false; then
 ./mk_group.sh output/sdk-s390x.list sdk-s390x osc/SUSE:SLE-11:GA/_product/sdk-s390x.group only_s390x
 fi
 
-./mk_group.sh output/opensuse/x11_cd.x86_64.list DVD osc/system:install:head/_product/DVD.group
+./mk_group.sh output/x11_cd.x86_64.list DVD osc/system:install:head/_product/DVD.group
 (cd osc/system:install:head/_product/ && osc ci -m "auto update")
 
 (cd osc/openSUSE:Factory:Live/package-lists-openSUSE; osc up -e; cp /package_lists/filelists.tar.bz2 .; osc ci -m "update from desdemona" .)
