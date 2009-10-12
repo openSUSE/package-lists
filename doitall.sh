dc108f939d6aaa3a334bfbfb59e122de58efa414
Stephan Kulow <coolo@suse.de>
2009-10-12 11:15:12 +0000

    no ppc atm

--- doitall.sh
+++ doitall.sh
@@ -46,15 +46,15 @@ set -e
 
 ./check_yast.sh output/opensuse/dvd-i586.list __i386__
 ./check_yast.sh output/opensuse/dvd-x86_64.list __x86_64__
-./check_yast.sh output/opensuse/dvd-ppc.list __powerpc__
+#./check_yast.sh output/opensuse/dvd-ppc.list __powerpc__
 
 (
 ./check_size.sh output/opensuse/dvd-i586.list i586
 ./check_size.sh output/opensuse/dvd-x86_64.list x86_64
-./check_size.sh output/opensuse/dvd-ppc.list ppc
+#./check_size.sh output/opensuse/dvd-ppc.list ppc
 ) | tee sizes
 
-./mk_group.sh output/opensuse/dvd-ppc.list DVD-ppc osc/openSUSE\:Factory/_product/DVD5-ppc.group only_ppc
+#./mk_group.sh output/opensuse/dvd-ppc.list DVD-ppc osc/openSUSE\:Factory/_product/DVD5-ppc.group only_ppc
 ./mk_group.sh output/opensuse/dvd-i586.list DVD-i586 osc/openSUSE\:Factory/_product/DVD5-i586.group only_i586
 ./mk_group.sh output/opensuse/dvd-x86_64.list DVD-x86_64 osc/openSUSE\:Factory/_product/DVD5-x86_64.group only_x86_64
 ./mk_group.sh output/opensuse/promo_dvd.i586.list REST-DVD-promo-i386 osc/openSUSE\:Factory/_product/DVD5-promo-i386.group
