39db91a0294eb8229f46aecd044729150b112821
Stephan Kulow <coolo@suse.de>
2011-02-25 11:30:00 +0000

    work on update test cases

--- update-tests/update.sh
+++ update-tests/update.sh
@@ -1,11 +1,21 @@
 rm -rf /tmp/myrepos
 : > drops.xml
+: > uninstalls.xml
 for i in `sed -e 's,.*>\(.*\)</ob.*,\1,' ../osc/openSUSE\:Factory/_product/obsoletepackages.inc`; do
    grep -xq $i $1.list || continue
    echo "<droporphaned kind=\"package\" name=\"$i\"/>" >> drops.xml
+   echo "<uninstall kind=\"package\" name=\"$i\"/>" >> uninstalls.xml 
 done
 
 sed -e '/!-- DROPS -->/r drops.xml' $1 > $1.tmp
 /usr/bin/deptestomatic $1.tmp 2> $1.error | tee $1.output | sed -n -e '1,/Other Valid Solution/p' | grep -v ' pattern:' | grep -v 'install product:' | grep '^>!>' | grep -e '^>!> \(install\|remove\|upgrade\) ' | sed -e 's,^>!> ,,; s, => .*,,; s,\[factor.*\].*,,; s,-[^-]*-[^-]*\.\(i.86\|noarch\)$,,'
+
+sed -e '/!-- DROPS -->/r uninstalls.xml' $1 > $1.uninstalls
+/usr/bin/deptestomatic $1.uninstalls 2> $1.error2 > $1.output2 
+list=`diff -u $1.output $1.output2 | grep "^+.*remove " | sed -e 's,.*remove  ,,; s,-[^-]*-[^-]*\.\(i.86\|noarch\|x86_64\)$,,'`
+for i in $list; do 
+  echo "<obsoletepackage>$i</obsoletepackage>" >&2
+done
+
 #rm $1.tmp
 #rm drops.xml
