ea74f998c96af42e4124112edcf931a7a717dcdb
Stephan Kulow <coolo@suse.de>
2011-09-01 12:08:29 +0000

    unlock gimp-help-browser

--- testtrack/update_full.sh
+++ testtrack/update_full.sh
@@ -27,7 +27,7 @@ do
      rarch=${i/obs-/}
      mkdir -p susex/$rarch
      echo -n "syncing $i "
-     count=`rsync -av $ignore --exclude *.meta --exclude *debuginfo* --exclude *debugsource* --exclude openSUSE-images* --exclude installation-images* --delete buildservice2.suse.de::opensuse-internal/build/openSUSE:Factory/standard/$rarch/:full/ susex/$rarch/ | grep .rpm | wc -l`
+     count=`rsync -av $ignore --exclude *.meta --exclude *debuginfo* --exclude *debugsource* --exclude openSUSE-images* --exclude installation-images* --delete backend-opensuse.suse.de::opensuse-internal/build/openSUSE:Factory/standard/$rarch/:full/ susex/$rarch/ | grep .rpm | wc -l`
      echo -n "found $count packages "
      if test "$count" = 0; then
         echo "done"
@@ -40,7 +40,7 @@ do
      rarch=${i/114-/}
      mkdir -p susex/$rarch
      echo -n "syncing $i "
-     count=`rsync -av $ignore --exclude *.meta --exclude *debuginfo* --exclude *debugsource* --exclude openSUSE-images* --exclude installation-images* --delete buildservice2.suse.de::opensuse-internal/build/openSUSE:11.4/standard/$rarch/:full/ susex/$rarch/ | grep .rpm | wc -l`
+     count=`rsync -av $ignore --exclude *.meta --exclude *debuginfo* --exclude *debugsource* --exclude openSUSE-images* --exclude installation-images* --delete backend-opensuse.suse.de::opensuse-internal/build/openSUSE:11.4/standard/$rarch/:full/ susex/$rarch/ | grep .rpm | wc -l`
      echo -n "found $count packages "
      if test "$count" = 0; then
         echo "done"
@@ -53,7 +53,7 @@ do
      rarch=${i/power-/}
      mkdir -p susex/$rarch
      echo -n "syncing $i "
-     count=`rsync -av $ignore --exclude *.meta --exclude *debuginfo* --exclude *debugsource* --exclude openSUSE-images* --exclude installation-images* --delete buildservice2.suse.de::opensuse-internal/build/openSUSE:Factory:PowerPC/standard/$rarch/:full/ susex/$rarch/ | grep .rpm | wc -l`
+     count=`rsync -av $ignore --exclude *.meta --exclude *debuginfo* --exclude *debugsource* --exclude openSUSE-images* --exclude installation-images* --delete backend-opensuse.suse.de::opensuse-internal/build/openSUSE:Factory:PowerPC/standard/$rarch/:full/ susex/$rarch/ | grep .rpm | wc -l`
      echo -n "found $count packages "
      if test "$count" = 0; then
         echo "done"
