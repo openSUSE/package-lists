571dd6c80dc9b0b6f728535bf3618ea5d3afb4d2
Stephan Kulow <coolo@suse.de>
2013-01-31 09:08:07 +0000

    switch to 12.3

--- testtrack/update_full.sh
+++ testtrack/update_full.sh
@@ -50,11 +50,24 @@ do
      fi
      ;;
 
-  122-*)
-     rarch=${i/122-/}
+  123-*)
+     rarch=${i/123-/}
      mkdir -p susex/$rarch
      echo -n "syncing $i "
-     count=`rsync --timeout=40 -av $ignore --exclude *.meta --exclude *debuginfo* --exclude *debugsource* --exclude openSUSE-images* --exclude installation-images* --delete backend-opensuse.suse.de::opensuse-internal/build/openSUSE:12.2/standard/$rarch/:full/ susex/$rarch/ | grep .rpm | wc -l`
+     count=`rsync --timeout=40 -av $ignore --exclude *.meta --exclude *debuginfo* --exclude *debugsource* --exclude openSUSE-images* --exclude installation-images* --delete backend-opensuse.suse.de::opensuse-internal/build/openSUSE:12.3/standard/$rarch/:full/ susex/$rarch/ | grep .rpm | wc -l`
+     echo -n "found $count packages "
+     if test "$count" = 0; then
+        echo "done"
+     else
+        touch dirty
+        echo
+     fi
+     ;;
+  nf-123-*)
+     rarch=${i/nf-123-/}
+     mkdir -p susex/$rarch
+     echo -n "syncing $i "
+     count=`rsync --timeout=40 -av $ignore --exclude *.meta --exclude *debuginfo* --exclude *debugsource* --exclude openSUSE-images* --exclude installation-images* --delete backend-opensuse.suse.de::opensuse-internal/build/openSUSE:12.3:NonFree/standard/$rarch/:full/ susex/$rarch/ | grep .rpm | wc -l`
      echo -n "found $count packages "
      if test "$count" = 0; then
         echo "done"
