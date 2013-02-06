60c732220341b86cd600330d6f1236e77fc55f0f
Stephan Kulow <coolo@suse.de>
2013-02-06 09:21:53 +0000

    trying to get the addon

--- langaddon.sh
+++ langaddon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 for i in i586 x86_64; do
-  diff output/opensuse/dvd-1.$i.list output/opensuse/dvd-addon_lang.$i.list | grep '^>' > output/opensuse/langaddon.$i.list
+  diff output/opensuse/dvd-1.$i.list output/opensuse/dvd-addon_lang.$i.list | grep '^>'  > output/opensuse/langaddon.$i.list
   LC_ALL=C sort -u output/opensuse/langaddon.$i.list | cut -d" " -f2 > t && mv t output/opensuse/langaddon.$i.list
 done
