e70011392bdfc9dff767ad6453ecaaecd3c86712
Stephan Kulow <coolo@suse.de>
2014-01-14 20:33:35 +0000

    add sudo

--- rebuildpacs.sh
+++ rebuildpacs.sh
@@ -63,7 +63,7 @@ osc api /build/$project/$repo/$arch/_builddepinfo > /tmp/builddep
 
 : > /tmp/torebuild
 touch $dir/package-lists-openSUSE
-for package in installation-images rpmlint rpmlint-mini bundle-lang-common bundle-lang-kde bundle-lang-gnome glib2-branding-openSUSE PackageKit-branding-openSUSE kiwi-config-openSUSE xfce4-branding-openSUSE kdebase4-openSUSE branding-openSUSE; do
+for package in installation-images-openSUSE rpmlint rpmlint-mini bundle-lang-common bundle-lang-kde bundle-lang-gnome glib2-branding-openSUSE PackageKit-branding-openSUSE kiwi-config-openSUSE xfce4-branding-openSUSE kdebase4-openSUSE branding-openSUSE; do
   osc buildinfo $project $package $repo $arch | grep 'bdep name' | grep -v rpmlint | grep -v wallpaper-branding > $dir/$package.new || true
   if diff -u $dir/$package.old $dir/$package.new | grep '^[+-]'; then
     echo $package >> /tmp/torebuild
