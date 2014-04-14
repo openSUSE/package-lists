cc689de7e69a225bb1a0a3a2dce305cc11220ce8
Stephan Kulow <coolo@suse.de>
2014-04-14 20:18:21 +0200

    more fixes

--- doit.sh
+++ doit.sh
@@ -16,27 +16,27 @@ for arch in i586 x86_64; do
        ;;
   esac
 
-    ./gen.sh opensuse/kde4_cd $arch
-    ./gen.sh opensuse/kde4_cd-default $arch
-    ./gen.sh opensuse/gnome_cd $arch
-    ./gen.sh opensuse/gnome_cd-default $arch 
+    ./gen.pl opensuse/kde4_cd $arch
+    ./gen.pl opensuse/kde4_cd-default $arch
+    ./gen.pl opensuse/gnome_cd $arch
+    ./gen.pl opensuse/gnome_cd-default $arch 
 
-    ./gen.sh opensuse/dvd-1 $arch
-    ./gen.sh opensuse/dvd-2 $arch
-    ./gen.sh opensuse/dvd-3 $arch
-    ./gen.sh opensuse/dvd-base $arch
-    ./gen.sh opensuse/dvd9 $arch
+    ./gen.pl opensuse/dvd-1 $arch
+    ./gen.pl opensuse/dvd-2 $arch
+    ./gen.pl opensuse/dvd-3 $arch
+    ./gen.pl opensuse/dvd-base $arch
+    ./gen.pl opensuse/dvd9 $arch
 
     cat output/opensuse/dvd-1.$arch.list output/opensuse/dvd-2.$arch.list output/opensuse/dvd-3.$arch.list output/opensuse/dvd-base.$arch.list | LC_ALL=C sort -u > output/opensuse/dvd-$arch.list
     cat output/opensuse/dvd-$arch.list output/opensuse/dvd9.$arch.list | LC_ALL=C sort -u > output/opensuse/dvd9-$arch.list
 
-    ./gen.sh opensuse/kde4_cd-base-default $arch
-    ./gen.sh opensuse/kde4_cd-unstable $arch
-    ./gen.sh opensuse/gnome_cd-nobundles $arch
-    ./gen.sh opensuse/kde4_cd-nobundles $arch
-    ./gen.sh opensuse/gnome_cd-x11-default $arch
-    ./gen.sh opensuse/x11_cd $arch
-    ./gen.sh opensuse/x11_cd-initrd $arch
+    ./gen.pl opensuse/kde4_cd-base-default $arch
+    ./gen.pl opensuse/kde4_cd-unstable $arch
+    ./gen.pl opensuse/gnome_cd-nobundles $arch
+    ./gen.pl opensuse/kde4_cd-nobundles $arch
+    ./gen.pl opensuse/gnome_cd-x11-default $arch
+    ./gen.pl opensuse/x11_cd $arch
+    ./gen.pl opensuse/x11_cd-initrd $arch
 
   if echo $file | grep -q -- "-default"; then
      for i in kernel-default powersave suspend OpenOffice_org-icon-themes smartmontools gtk-lang gimp-lang vte-lang icewm-lite yast2-trans-en_US bundle-lang-common-en opensuse-manual_en bundle-lang-kde-en bundle-lang-gnome-en openSUSE-release openSUSE-release-ftp kernel-default-base kernel-default-extra smolt virtualbox-ose-kmp-default ndiswrapper-kmp-default preload-kmp-default tango-icon-theme oxygen-icon-theme mono-core marble-data gnome-packagekit Mesa libqt4-x11 gnome-icon-theme xorg-x11-fonts-core ghostscript gio-branding-upstream grub grub2 grub2-branding-openSUSE plymouth-branding-openSUSE kdebase4-workspace-branding-openSUSE kdebase4-workspace libQtWebKit4 opensuse-startup_en glibc-locale; do
@@ -48,13 +48,13 @@ for arch in i586 x86_64; do
       grep -vx openSUSE-release-ftp output/$file.$GEN_ARCH.list > t && mv t output/$file.$GEN_ARCH.list
   fi
 
-    ./gen.sh opensuse/dvd-nonoss $arch
+    ./gen.pl opensuse/dvd-nonoss $arch
     
     diff -u output/opensuse/kde4_cd.$arch.list output/opensuse/kde4_cd-default.$arch.list | grep -v +++ | grep ^+
     diff -u output/opensuse/gnome_cd.$arch.list output/opensuse/gnome_cd-default.$arch.list | grep -v +++ | grep ^+
     
-    ./gen.sh opensuse/promo_dvd $arch
-    ./gen.sh opensuse/dvd-addon_lang $arch
+    ./gen.pl opensuse/promo_dvd $arch
+    ./gen.pl opensuse/dvd-addon_lang $arch
 done
 
 ./nonoss.sh
--- doitall.sh
+++ doitall.sh
@@ -5,16 +5,6 @@ git pull
 . ./options
 
 (cd osc/openSUSE\:Factory/_product/ && osc up)
-cd testtrack/
-./update_full.sh $tree-i586 $tree-x86_64 nf-$tree-i586 nf-$tree-x86_64
-echo -n "updating patterns "
-if ./unpack_patterns.sh $diffonly > patterns.log 2>&1; then
-   touch ../dirty
-   echo "done"
-else
-   echo "unchanged"
-fi 
-cd ..
 osc api "/build/openSUSE:$proj/_result?package=bash&repository=standard" > /tmp/state
 if grep -q 'dirty="true"' /tmp/state || grep -q 'state="building"' /tmp/state; then
    echo "standard still dirty"
@@ -22,14 +12,7 @@ if grep -q 'dirty="true"' /tmp/state || grep -q 'state="building"' /tmp/state; t
      exit 0
    fi
 fi
-# now sync again
-cd testtrack
-WITHDESCR=1 ./update_full.sh $tree-i586 nf-$tree-i586 $tree-x86_64 nf-$tree-x86_64 || touch ../dirty
-cd ..
-
-if test -f dirty; then
-  ./doit.sh
-fi
+./doit.sh
 
 cd update-tests
 test -f ../dirty && ./testall.sh
@@ -42,12 +25,6 @@ for f in output/opensuse/*.list; do
     cp -a $saved $f
   fi
 done 
-
-if test -f dirty; then
-   ./gen.sh opensuse/x11_cd-boottest x86_64
-
-   cp -a output/opensuse/*.list saved
-fi
 
 set -e
 
