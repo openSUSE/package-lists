3825f2ba0fe8199292aeb65cf8ea6660239d2082
Stephan Kulow <coolo@suse.de>
2014-04-14 13:07:32 +0200

    test script

---
+++ gen.pl
@@ -0,0 +1,42 @@
+#! /usr/bin/perl
+
+use File::Basename;
+
+my $file = $ARGV[0] || die 'need file';
+my $arch = $ARGV[1] || die 'need arch';
+
+sub read_file_recursively($) {
+  my $tfile = shift;
+  my @lines;
+  open(my $fh, '<', $tfile);
+
+  while ( <$fh> ) {
+    chomp;
+
+    my $line = $_;
+
+    if ($line =~ m/^#INCLUDE\s*(\S+)/) {
+      push(@lines, "\n# from $1\n");
+      push(@lines, read_file_recursively(dirname($file) . "/" . $1));
+      push(@lines, "# end $1\n\n");
+      next;
+    }
+
+    if ($line =~ m/#.*!$arch/) {
+      print "skipping $line\n";
+      next;
+    }
+    push(@lines, $line);
+  }
+
+  close($fh);
+
+  return @lines;
+}
+
+open(OUT, ">t");
+print OUT "repo openSUSE:Factory-standard-$arch 0 solv trees/openSUSE:Factory-standard-$arch.solv\n";
+for my $line (read_file_recursively($file)) {
+  print OUT "$line\n";
+}
+close(OUT);
