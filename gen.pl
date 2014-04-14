e95eba2cc8ad95c882954b8a23fa619fb35fb42a
Stephan Kulow <coolo@suse.de>
2014-04-14 19:33:02 +0200

    fix INCLUDES

--- gen.pl
+++ gen.pl
@@ -23,7 +23,6 @@ sub read_file_recursively($) {
     }
 
     if ($line =~ m/#.*!$arch/) {
-      print "skipping $line\n";
       next;
     }
     push(@lines, $line);
@@ -40,3 +39,22 @@ for my $line (read_file_recursively($file)) {
   print OUT "$line\n";
 }
 close(OUT);
+
+open(TS, "testsolv -r t|");
+my @installs;
+while ( <TS> ) {
+  if (/^install (.*)-[^-]+-[^-]+\.(\S*)@/) {
+	push(@installs, $1);
+	print "T $1\n";
+  } else {
+	print $_;
+	exit(1);
+  }
+}
+close(TS);
+open(OUT, ">", "output/$file.$arch.list");
+for my $pkg (sort @installs) {
+  print OUT "$pkg\n";
+}
+close(OUT);
+
