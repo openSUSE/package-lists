9bb29e41f539778e4bebe6ae36c088ad285ad8f9
Ludwig Nussel <ludwig.nussel@suse.de>
2014-08-19 10:44:53 +0200

    replace update_supplements.sh with libsolv script

--- update_supplements.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-grep Supplements: /work/SRC/all/*/*.spec | grep -v packageand | grep '('   | sed -e 's,.*Supplements: *,,' | tr ' ' '\012' | sed -e 's,([^:]*:\(.*\),(\1,' | \
-   sort  -u | sed -e 's,^,namespace namespace:,; s,$, @SYSTEM,' > opensuse/all-supplements
