a926edd55cb15927be329a5fb3e3ca4e0b6138a7
Ludwig Nussel <ludwig.nussel@suse.de>
2014-08-20 13:58:00 +0200

    move to subdir

--- langaddon.sh
+++ langaddon.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+: ${proj:=Factory}
+
 for i in i586 x86_64; do
-  diff output/opensuse/dvd-1.$i.list output/opensuse/dvd-addon_lang.$i.list | grep '^>'  > output/opensuse/langaddon.$i.list
-  LC_ALL=C sort -u output/opensuse/langaddon.$i.list | cut -d" " -f2 > t && mv t output/opensuse/langaddon.$i.list
+  diff output/opensuse/$proj/dvd-1.$i.list output/opensuse/$proj/dvd-addon_lang.$i.list | grep '^>'  > output/opensuse/$proj/langaddon.$i.list
+  LC_ALL=C sort -u output/opensuse/$proj/langaddon.$i.list | cut -d" " -f2 > t && mv t output/opensuse/$proj/langaddon.$i.list
 done
