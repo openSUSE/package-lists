efefedca496f648d46b3574d9b1ec0cd5fe28c7d
Your Name <you@example.com>
2016-08-15 16:18:11 +0200

    make executable, fix for Leap

--- create-drop-list/create-distro.sh
+++ create-drop-list/create-distro.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 mkdir -p $1/suse/setup/descr
-wget http://download.opensuse.org/distribution/$1/repo/oss/content -O $1/content
-wget http://download.opensuse.org/distribution/$1/repo/oss/suse/setup/descr/packages.gz -O $1/suse/setup/descr/packages.gz
+wget http://download.opensuse.org/distribution/leap/$1/repo/oss/content -O $1/content
+wget http://download.opensuse.org/distribution/leap/$1/repo/oss/suse/setup/descr/packages.gz -O $1/suse/setup/descr/packages.gz
 susetags2solv -X -c $1/content -d $1/suse/setup/descr/ > $1.repo.oss.solv
 rm -rf $1
 mkdir -p $1/suse/setup/descr
 
-wget http://download.opensuse.org/distribution/$1/repo/non-oss/content -O $1/content
-wget http://download.opensuse.org/distribution/$1/repo/non-oss/suse/setup/descr/packages.gz -O $1/suse/setup/descr/packages.gz
+wget http://download.opensuse.org/distribution/leap/$1/repo/non-oss/content -O $1/content
+wget http://download.opensuse.org/distribution/leap/$1/repo/non-oss/suse/setup/descr/packages.gz -O $1/suse/setup/descr/packages.gz
 susetags2solv -X -c $1/content -d $1/suse/setup/descr/ > $1.repo.nonoss.solv
 
 rm -rf $1
