cb914439f7a2b41fae6e30bf0592e75373d6ac9b
BOT Admin <botadmin@opensuse.org>
2017-02-17 21:04:21 +0100

    Make langaddon.sh parallel executable by using different temp files

--- langaddon.sh
+++ langaddon.sh
@@ -4,5 +4,5 @@
 
 for i in i586 x86_64; do
   diff output/opensuse/$proj/dvd-1.$i.list output/opensuse/$proj/dvd-addon_lang.$i.list | grep '^>'  > output/opensuse/$proj/langaddon.$i.list
-  LC_ALL=C sort -u output/opensuse/$proj/langaddon.$i.list | cut -d" " -f2 > t && mv t output/opensuse/$proj/langaddon.$i.list
+  LC_ALL=C sort -u output/opensuse/$proj/langaddon.$i.list | cut -d" " -f2 > output/opensuse/$proj/langaddon.$i.list.tmp && mv output/opensuse/$proj/langaddon.$i.list.tmp output/opensuse/$proj/langaddon.$i.list
 done
