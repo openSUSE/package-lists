4c125c89ae66a1b316e9412a2b5db40341d93461
BOT Admin <botadmin@opensuse.org>
2017-11-17 17:00:11 +0100

    auto commit for Factory/standard

--- create-drop-list.sh
+++ create-drop-list.sh
@@ -11,4 +11,11 @@ susetags2solv -d MANUAL_OBSOLETES > MANUAL_OBSOLETES.solv
 		 $proj/*.solv \
                  *.solv > ../osc/openSUSE:$proj/$product/obsoletepackages.inc
 cd ../osc/openSUSE:$proj/$product
-osc ci -m "updated drop list"
+if [ "$(osc status | wc -l)" -gt 0 ]; then
+  osc ci -m "updated drop list"
+  # Try to abort a running build - since we change the drop list, openSUSE-release will
+  # be rebuilt anyway, invalidating the currently running build of the FTP Tree
+  # Also all DVD builds might be invalid, but comapred to the build time of the FTP tree
+  # they are negelctable
+  osc abortbuild openSUSE:$proj/_product:openSUSE-ftp-ftp-i586_x86_64/images/local
+fi
