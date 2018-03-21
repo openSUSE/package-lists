45098dcfe2c3092b2f936511aad2ec85af62162f
BOT Admin <botadmin@opensuse.org>
2018-03-21 16:12:20 +0100

    Abort logic no longer needed

--- create-drop-list.sh
+++ create-drop-list.sh
@@ -11,12 +11,3 @@ susetags2solv -d MANUAL_OBSOLETES > MANUAL_OBSOLETES.solv
 		 $proj/*.solv \
                  *.solv > ../osc/openSUSE:$proj/$product/obsoletepackages.inc
 cd ../osc/openSUSE:$proj/$product
-if [ "$(osc status | wc -l)" -gt 0 ]; then
-  osc ci -m "updated drop list"
-  # Try to abort a running build - since we change the drop list, openSUSE-release will
-  # be rebuilt anyway, invalidating the currently running build of the FTP Tree
-  # Also all DVD builds might be invalid, but comapred to the build time of the FTP tree
-  # they are negelctable
-  echo "Attemptint to abortbuild openSUSE:$proj/000product:openSUSE-ftp-ftp-i586_x86_64/images/local"
-  osc abortbuild openSUSE:$proj/000product:openSUSE-ftp-ftp-i586_x86_64/images/local
-fi
